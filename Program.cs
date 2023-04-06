e3b260d0ea57feaf1c8a9700aa3bd761448ddef3
dontlikeouy <diyar_2017@bk.ru>
2023-04-06 11:55:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml;
+
+Console.WriteLine("Hello, World!");
+using (XmlReader reader = XmlReader.Create(@"C:\Users\cook\Downloads\dsad.xml"))
+{
+    while (reader.Read())
+    {
+        Console.WriteLine($"Type: {reader.NodeType} LocalName: {reader.LocalName}, Value: {reader.Value}");
+
+    }
+}
